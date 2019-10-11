635994efdf9b5c0457bd176022e09fc84353522d
MrWang-M <654120914@qq.com>
2019-10-11 17:10:50 +0800

    啊啊啊啊

--- src/main/java/com/qfedu/shiroDemo/utils/MyRealm.java
+++ src/main/java/com/qfedu/shiroDemo/utils/MyRealm.java
@@ -34,6 +34,7 @@ public class MyRealm extends AuthorizingRealm {
     protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
         String username = (String)principalCollection.iterator().next();
 
+        //测试
 
         SimpleAuthorizationInfo info = new SimpleAuthorizationInfo();
         info.setRoles(roleDAO.queryRolesByUserName(username));
